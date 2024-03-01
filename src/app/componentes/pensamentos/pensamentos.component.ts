79dc97c933433edcfc92b193a5de7d37de350007
rodovalhovic <victorialicia22@gmail.com>
2024-03-01 11:18:10 -0300

    componentes cabeçalho, pensamentos e rodapé

---
+++ src/app/componentes/pensamentos/pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamentos',
+  templateUrl: './pensamentos.component.html',
+  styleUrls: ['./pensamentos.component.css']
+})
+export class PensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
